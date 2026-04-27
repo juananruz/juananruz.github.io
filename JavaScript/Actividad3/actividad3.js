3f57592930a65cad6dd8f1d498425b942331bb17
juananruz <jruzpanadero@gmail.com>
2026-04-27 19:25:15 +0200

    Add files via upload

--- JavaScript/Actividad3/actividad3.js
+++ JavaScript/Actividad3/actividad3.js
@@ -1,5 +1,23 @@
-function mostrarEstacion(){
- let estaciones=["Invierno","Primavera","Verano","Otoño"];
- let n=document.getElementById("numero").value;
- document.getElementById("resultado").innerHTML="<b>"+estaciones[n-1]+"</b>";
+function mostrarEstacion() {
+    let n = document.getElementById("numero").value;
+    let estacion = "";
+
+    switch (n) {
+        case "1":
+            estacion = "Invierno";
+            break;
+        case "2":
+            estacion = "Primavera";
+            break;
+        case "3":
+            estacion = "Verano";
+            break;
+        case "4":
+            estacion = "Otoño";
+            break;
+        default:
+            estacion = "Número incorrecto";
+    }
+
+    document.getElementById("resultado").innerHTML = "<b>" + estacion + "</b>";
 }
